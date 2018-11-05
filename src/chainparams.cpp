85da35fda3f5426369b1fef77ce7594afed7d703
EULO <kingsfinancenz18@gmail.com>
2018-11-05 13:03:06 +0800

    fix domain error.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -153,9 +153,9 @@ public:
         assert(hashGenesisBlock == uint256("0x00000013ea705af4ed8b40a86250bc6d78b982c604eef80a908cec7f11abbdd1"));
         assert(genesis.hashMerkleRoot == uint256("0x4df5d8d4db708df49c4f911a0946991d5a7628e2e93dfe22b76f9233a9492426"));
 
-        vSeeds.push_back("seed1.eulo.io");          // seed1
-        vSeeds.push_back("seed2.eulo.io");          // seed2
-        vSeeds.push_back("seed3.eulo.io");          // seed3
+        vSeeds.push_back(CDNSSeedData("seed1.eulo.io", "seed1.eulo.io"));       // seed1
+        vSeeds.push_back(CDNSSeedData("seed2.eulo.io", "seed2.eulo.io"));       // seed2
+        vSeeds.push_back(CDNSSeedData("seed3.eulo.io", "seed3.eulo.io"));       // seed3
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 68);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 13);
@@ -245,9 +245,9 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
 
-        vSeeds.push_back("seed1.eulo.io");          // seed1
-        vSeeds.push_back("seed2.eulo.io");          // seed2
-        vSeeds.push_back("seed3.eulo.io");          // seed3
+        vSeeds.push_back(CDNSSeedData("seed1.eulo.io", "seed1.eulo.io"));       // seed1
+        vSeeds.push_back(CDNSSeedData("seed2.eulo.io", "seed2.eulo.io"));       // seed2
+        vSeeds.push_back(CDNSSeedData("seed3.eulo.io", "seed3.eulo.io"));       // seed3
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 130); // Testnet ulo addresses start with 'u'
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 19);  // Testnet eulo script addresses start with '8' or '9'
