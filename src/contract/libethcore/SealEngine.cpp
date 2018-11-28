dd318fae229bc9526977e3ce24ce12b8c674bd78
EULO <kingsfinancenz18@gmail.com>
2018-11-28 17:52:49 +0800

    add endian.h byteswap for mingw complier

--- src/contract/libethcore/SealEngine.cpp
+++ src/contract/libethcore/SealEngine.cpp
@@ -60,7 +60,7 @@ SealEngineFace* SealEngineRegistrar::create(ChainOperationParams const& _params)
 
 EVMSchedule const& SealEngineBase::evmSchedule(EnvInfo const& _envInfo) const
 {
-	////////////////////////////////////////////////////////// // qtum
+    ////////////////////////////////////////////////////////// // eulo
 	if (u256(0) == chainParams().u256Param("EIP158ForkBlock") && 
 		u256(0) == chainParams().u256Param("EIP150ForkBlock") &&
 		u256(0) == chainParams().u256Param("homsteadForkBlock")){
